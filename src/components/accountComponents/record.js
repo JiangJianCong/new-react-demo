97fc2b7e78d35f1a8ac9c222f8c10cb6716f81a4
space <910455361@qq.com>
2018-05-09 11:07:35 +0800

    A-新增删除功能

--- src/components/accountComponents/record.js
+++ src/components/accountComponents/record.js
@@ -1,16 +1,94 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import * as RecordsAPI from '../../utils/RecordApi'
 
 
 export default class Record extends React.Component {
-  render() {
+
+  constructor() {
+    super()
+    this.state = {
+       edit : false
+    }
+  }
+  handleToggle(){
+    this.setState({
+      edit : !this.state.edit
+    })
+  }
+
+  /**
+   * 处理编辑数据，调用update的api
+   * @param event
+   */
+  handleEdit(event) {
+    event.preventDefault()
+    const record = {
+      date : this.refs.date.value,
+      title : this.refs.title.value,
+      amount : Number.parseInt(this.refs.amount.value,0)
+
+    }
+    RecordsAPI.update(this.props.record.id,record).then(
+      response => {
+        this.setState({
+          edit : false
+        })
+        this.props.handleEditRecord(this.props.record,response.data)
+      }
+    ).catch(
+      error => console.log(error.message)
+    )
+
+  }
+
+  /**
+   * 删除按钮监听的函数
+   * @param event
+   */
+  handleDelete(event){
+    event.preventDefault();
+    RecordsAPI.remove(this.props.record.id).then(
+      response => this.props.handleDeleteRecord(this.props.record)
+    ).catch(
+      error => console.log(error.message)
+    )
+  }
+
+  recordRow() {
     return (
       <tr>
-        <td>{this.props.date}</td>
-        <td>{this.props.title}</td>
-        <td>{this.props.amount}</td>
+        <td>{this.props.record.date}</td>
+        <td>{this.props.record.title}</td>
+        <td>{this.props.record.amount}</td>
+        <td>
+          <button className="btn btn-info mr-1" onClick={this.handleToggle.bind(this)}>Edit</button>
+          <button className="btn btn-danger" onClick={this.handleDelete.bind(this)}>Delete</button>
+        </td>
       </tr>
     )
+  }
+
+  recordForm() {
+    return (
+      <tr>
+        <td><input type="text" className="form-control" defaultValue={this.props.record.date} ref='date'/></td>
+        <td><input type="text" className="form-control" defaultValue={this.props.record.title} ref='title'/></td>
+        <td><input type="text" className="form-control" defaultValue={this.props.record.amount} ref='amount'/></td>
+        <td>
+          <button className="btn btn-info mr-1" onClick={this.handleEdit.bind(this)}>Update</button>
+          <button className="btn btn-danger" onClick={this.handleToggle.bind(this)}>Cancel</button>
+        </td>
+      </tr>
+    )
+  }
+
+  render() {
+    if (this.state.edit) {
+      return this.recordForm()
+    }else {
+      return this.recordRow()
+    }
   }
 }
 
